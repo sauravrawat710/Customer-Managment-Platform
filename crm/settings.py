f66d6ccbc8e286a8bde5d923c663cf8bb4db96e6
Saurav Singh Rawat <63958336+sauravrawat710@users.noreply.github.com>
2020-07-21 10:14:51 +0530

    Update settings.py

--- crm/settings.py
+++ crm/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+n@ep!*n_rmgzr2*$=(dwc&=1s(2zii8wmgj!atc6lazsg93re'
+SECRET_KEY = 'Your_Secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -139,5 +139,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'sauravrawatsp@gmail.com'
-EMAIL_HOST_PASSWORD = 'chutiya1234'
+EMAIL_HOST_USER = 'Your Email Address'
+EMAIL_HOST_PASSWORD = 'Your Pawsword'
